9d449bb00d397a82394c8e44fb551d9f4864b7f5
BagusFary <bagusfaryananta@gmail.com>
2023-10-21 14:18:16 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\AboutUsController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProgramController;
-use App\Http\Controllers\ContactUsController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,13 +15,3 @@ use App\Http\Controllers\ContactUsController;
 */
 
 Route::get('/', [HomeController::class, 'index']);
-Route::prefix('products')->group(function(){
-    
-});
-Route::get('/news/{id}', [NewsController::class, 'index']);
-Route::prefix('program')->group(function(){
-
-});
-Route::get('/about', [AboutUsController::class, 'index']);
-Route::get('/contact', [ContactUsController::class, 'index']);
-
